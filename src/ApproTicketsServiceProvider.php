8fb95ec476dafdf0a85a4be7f33b1d962f2bcfe3
Eladi Sánchez <eladisanchez@gmail.com>
2024-06-07 21:08:05 +0200

    chore: Update Filament panel configuration in ApproTicketsServiceProvider

--- src/ApproTicketsServiceProvider.php
+++ src/ApproTicketsServiceProvider.php
@@ -4,7 +4,6 @@ namespace ApproTickets;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
-use Filament\PanelProvider;
 use Filament\Panel;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -16,61 +15,62 @@ use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Facades\Filament;
 
 
-class ApproTicketsServiceProvider extends PanelProvider
+class ApproTicketsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->publishes([
-            __DIR__.'/config/tickets.php' => config_path('tickets.php'),
+            __DIR__ . '/config/tickets.php' => config_path('tickets.php'),
         ], 'config');
 
         // Filament
         Model::unguard();
+
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/config/tickets.php', 'tickets');
+        $this->mergeConfigFrom(__DIR__ . '/config/tickets.php', 'tickets');
+        
+        Filament::registerPanel(
+            fn(): Panel => $panel
+                ->default()
+                ->id('admin')
+                ->path('admin')
+                ->login()
+                ->discoverResources(in: __DIR__ . '/Filament/Resources', for: 'ApproTickets\\Filament\\Resources')
+                ->discoverPages(in: __DIR__ . '/Filament/Resources', for: 'ApproTickets\\Filament\\Pages')
+                ->pages([])
+                ->discoverWidgets(in: __DIR__ . '/Filament/Widgets', for: 'ApproTickets\\Filament\\Widgets')
+                ->middleware([
+                    EncryptCookies::class,
+                    AddQueuedCookiesToResponse::class,
+                    StartSession::class,
+                    AuthenticateSession::class,
+                    ShareErrorsFromSession::class,
+                    VerifyCsrfToken::class,
+                    SubstituteBindings::class,
+                    DisableBladeIconComponents::class,
+                    DispatchServingFilamentEvent::class,
+                ])
+                ->authMiddleware([
+                    Authenticate::class,
+                ])
+                ->favicon(asset('favicon.png'))
+                ->font('Montserrat')
+        );
     }
 
     protected function getResources(): array
     {
         return [
-            Filament\Resources\ProductResource::class,
+            \ApproTickets\Filament\Resources\ProductResource::class,
         ];
-    }
-
-    public function panel(Panel $panel): Panel
-    {
-        return $panel
-            ->default()
-            ->id('admin')
-            ->path('admin')
-            ->login()
-            ->discoverResources(in: __DIR__.'/Filament/Resources', for: 'ApproTickets\\Filament\\Resources')
-            ->discoverPages(in: __DIR__.'/Filament/Resources', for: 'ApproTickets\\Filament\\Pages')
-            ->pages([])
-            ->discoverWidgets(in: __DIR__.'/Filament/Widgets', for: 'ApproTickets\\Filament\\Widgets')
-            ->middleware([
-                EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
-                StartSession::class,
-                AuthenticateSession::class,
-                ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
-                SubstituteBindings::class,
-                DisableBladeIconComponents::class,
-                DispatchServingFilamentEvent::class,
-            ])
-            ->authMiddleware([
-                Authenticate::class,
-            ])
-            ->favicon(asset('favicon.png'))
-            ->font('Montserrat');
     }
 
 }

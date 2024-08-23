38fce0af4152bb657b1d83d07d62702551991d0f
Eladi Sánchez <eladisanchez@gmail.com>
2024-08-23 16:47:25 +0200

    add styles to map template

--- src/resources/views/filament/resources/venue-resource/widgets/location-map.blade.php
+++ src/resources/views/filament/resources/venue-resource/widgets/location-map.blade.php
@@ -1,5 +1,33 @@
 <x-filament-widgets::widget>
     <x-slot name="heading">Localitats</x-slot>
+    <style>
+        .map {
+            width: 800px;
+            display: grid;
+            gap: 1px;
+            background: #eee;
+            border: 1px solid #eee;
+            font-family: 'arial', sans-serif;
+        }
+
+        .seat {
+            aspect-ratio: 1;
+            background: #FFF;
+            position: relative;
+            font-size: 9px;
+            line-height: 8px;
+            text-align: center;
+        }
+
+        .seat.selected {
+            background: #deec9a;
+        }
+
+        .seat strong,
+        .seat span {
+            display: block;
+        }
+    </style>
     <x-filament::section>
         <div class="flex gap-4">
             <div class="map" style="grid-template-columns: repeat(45, 1fr);">
@@ -34,7 +62,8 @@
                 </div>
                 <div class="flex gap-3">
                     <x-filament::button wire:click="handleSave">Desa el plànol</x-filament::button>
-                    <x-filament::button wire:click="handleDelete" color="danger" icon="heroicon-m-trash">Esborra-ho tot</x-filament::button>
+                    <x-filament::button wire:click="handleDelete" color="danger" icon="heroicon-m-trash">Esborra-ho
+                        tot</x-filament::button>
                 </div>
             </div>
         </div>
